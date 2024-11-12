b584e7ba51630fe8577c2115cd42ed70538f7452
KiLcRaFt <killcraft96@gmail.com>
2024-11-12 16:08:27 +0200

    Convert XML to json file

---
+++ convert_to_json.php
@@ -0,0 +1,20 @@
+<?php
+
+$xmlFile = 'Kompaania.xml';
+$jsonFile = 'Kompaania.json';
+
+if (file_exists($xmlFile)) {
+
+    $xml = simplexml_load_file($xmlFile);
+
+    // XML-i konverteerimine JSON-i
+    $json = json_encode($xml, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+    // JSON-i salvestamine faili
+    file_put_contents($jsonFile, $json);
+
+    header('Content-type: application/json');
+    echo $json;
+} else {
+    exit('Не удалось открыть Kompaania.xml.');
+}
